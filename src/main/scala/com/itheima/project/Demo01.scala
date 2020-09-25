dd9cd280dd3efd7322f76c579dc10beb48fe5493
glzhang <17601346955@163.com>
2020-09-25 08:53:07 +0800

    Initial commit

---
+++ src/main/scala/com/itheima/project/Demo01.scala
@@ -0,0 +1,47 @@
+package com.itheima.project
+
+/**
+  * java 定义类，然后在类中定义一个静态的main方法
+  *
+  * scala:没有静态的概念，创建对象在对象中定义main方法
+  *
+  * 关联源码:
+  *
+  *
+  * java中变量：  int  a = 10;
+  *  final int A = 10 常量
+  *  final class 没有后代的 类，这个类不能有子类
+  *
+  *  scala:
+  *  在scala中能有常量的地方不用 变量
+  *  常量 安全！！
+  *
+  *
+  *
+  *
+  *
+  */
+object Demo01 {
+
+  def main(args: Array[String]): Unit = {
+
+    //定义一个变量
+//    var a:Int = 10
+//    a = 20
+//    //常量
+//    val b = 20
+//    println(b)
+//    println(a)//a.sout
+//var a: Int = 10 类型推导
+    //万能健: alt + enter
+    
+
+
+
+
+
+
+  }
+
+
+}

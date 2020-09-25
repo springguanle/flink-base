9ee81a8f082b21c03c8d4fe4f2141da507aac67c
glzhang <17601346955@163.com>
2020-09-25 09:02:33 +0800

    输出字符串

---
+++ src/main/scala/com/itheima/project/OutPutString.scala
@@ -0,0 +1,27 @@
+package com.itheima.project
+
+object OutPutString {
+
+  def main(args: Array[String]): Unit = {
+
+    //字符串的输出
+    /**
+      * java的输出 System.out.println("")
+      *
+      * scala的输出: print("")
+      *
+      * %d 输出整数
+      * %s 输出字符串
+      * %f 浮点数
+      *
+      */
+
+     val a =20
+
+    println("格式化输出 %d",a)
+
+
+  }
+
+
+}
